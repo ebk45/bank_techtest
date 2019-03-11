40d43491c6622166342be56272107f3526b77fd9
ellie turnock <ebturnock@gmail.com>
2019-03-11 13:58:07 +0000

    :pencil2: improved naming

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe 'Bank' do
   context '#balance' do
     it 'should print balance to the console' do
       account = Bank.new
-      expect{account.balance}.to output("0").to_stdout
+      expect{account.show_balance}.to output("0").to_stdout
     end
   end
 
   context '#deposit' do
     it 'should add the amount to the balance' do
-      account = Bank.new
-      expect{account.deposit(10)}.to change {account.balance}.by(10)
+      acc = Bank.new
+      expect{ acc.deposit(10) }.to change { acc.balance }.by(+10)
     end
   end
 end
