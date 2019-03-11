35e0c6d34ae97fa1f1165ecab3ffe8551a99a7d3
ellie turnock <ebturnock@gmail.com>
2019-03-11 12:47:06 +0000

    :white_check_mark: Test: balance should print to console

---
+++ spec/bank_spec.rb
@@ -0,0 +1,11 @@
+
+
+RSpec.describe 'Bank' do
+
+  context '#balance' do
+    it 'should print balance to the console' do
+      account = Bank.new()
+      expect{account.balance}.to output(0).to_stdout
+    end
+  end
+end
