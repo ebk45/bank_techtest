0b53f903f23df90765f58ddd3872967c1b043429
ellie turnock <ebturnock@gmail.com>
2019-03-11 13:00:07 +0000

    :white_check_mark: updated test for different output requirement

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,11 +1,11 @@
-
+require './lib/bank'
 
 RSpec.describe 'Bank' do
 
   context '#balance' do
     it 'should print balance to the console' do
       account = Bank.new()
-      expect{account.balance}.to output(0).to_stdout
+      expect{account.balance}.to output("0").to_stdout
     end
   end
 end
