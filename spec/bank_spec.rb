a85b786222176f2ddf3fe854af47da5868720a32
ellie turnock <ebturnock@gmail.com>
2019-03-11 13:10:54 +0000

    :white_check_mark: Test: deposit function adds amount to balance

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,8 +4,15 @@ RSpec.describe 'Bank' do
 
   context '#balance' do
     it 'should print balance to the console' do
-      account = Bank.new()
+      account = Bank.new
       expect{account.balance}.to output("0").to_stdout
+    end
+  end
+
+  context '#deposit' do
+    it 'should add the amount to the balance' do
+      account = Bank.new
+      expect{account.deposit(10)}.to change {account.balance}.by(10)
     end
   end
 end
