599bff4e58ffbecd7b29f853848fc1cf9ace6f24
ellie turnock <ebturnock@gmail.com>
2019-03-13 14:32:30 +0000

    :recycle: creates new Bank before each test

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,32 +2,32 @@ require './lib/bank'
 
 RSpec.describe 'Bank' do
 
+  before :all do
+    @account = Bank.new
+  end
+
   context '#balance' do
     it 'should print balance to the console' do
-      account = Bank.new
-      expect{account.show_balance}.to output("0").to_stdout
+      expect{@account.show_balance}.to output("0").to_stdout
     end
   end
 
   context '#deposit' do
     it 'should add the amount to the balance' do
-      acc = Bank.new
-      expect{ acc.deposit(10) }.to change { acc.balance }.by(+10)
+      expect{ @account.deposit(10) }.to change { @account.balance }.by(+10)
     end
   end
 
   context '#withdraw' do
     it 'should deduct the amount from the balance' do
-      acc = Bank.new
-      expect{ acc.withdraw(10) }.to change { acc.balance }.by(-10)
+      expect{ @account.withdraw(10) }.to change { @account.balance }.by(-10)
     end
   end
 
   context '#transaction_history' do
     it 'should store deposits in the transaction history' do
-      acc = Bank.new
-      acc.deposit(10)
-      expect(acc.transaction_history).to include(10)
+      @account.deposit(10)
+      expect(@account.transaction_history).to include(10)
     end
   end
 end
